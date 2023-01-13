a09d29b12b9006f368f3a92b72d705096712ff30
HuzaifaTahir <huzaifatahir332@gmail.com>
2023-01-13 11:21:58 +0500

    create new file for stablish SSH

---
+++ SNDRC/CISCO/router/access_cisco_router/access_cisco_router_ssh.py
@@ -0,0 +1,53 @@
+from netmiko import ConnectHandler
+import os
+
+
+# for i in range(1, 4):
+#     os.chdir("..")
+# print(os.getcwd())
+with open("login_info.txt", "r") as f:
+    var = f.readline()
+    info = var.split(",")
+cisco_router = {
+    'device_type': 'cisco_ios',
+    'host': info[0],
+    'username': info[1],
+    'password': info[2],
+    'secret': info[3],
+    'port': 22,
+}
+class SshDevice:
+    def __init__(self):
+        self.ssh = ConnectHandler(**cisco_router)
+        self.ssh.enable()
+    def add_commands(self, commands):
+        self.ssh.config_mode()
+        self.ssh.send_command(commands)
+        self.ssh.exit_config_mode()
+    def save_device_commands(self):
+        result = self.ssh.send_command("copy run start\n")
+        print(result)
+        self.ssh.send_command("\n")
+        # self.telnet.read_until(b'Overwrite the previous NVRAM configuration?[confirm]', 0.3)
+        # self.telnet.write(b'\n')
+    def backup_device_commands(self):
+        self.ssh.send_command("terminal length 0\n")
+        result = self.ssh.send_command("show running-config")
+        print(result)
+
+    def get_info_from_router(self, command):
+        # self.telnet.write(command.encode('ascii') + b'\n')
+        # all = self.telnet.read_very_eager().decode('utf-8')
+        # print(all)
+        # self.telnet.write(b'end\n')
+        # self.telnet.write(b'exit\n')
+        return "all"
+
+    def enable(self):
+        self.ssh.enable()
+        result = self.ssh.send_command('show run')
+        return result
+
+conn = SshDevice()
+# result = c1.enable()
+# print(result)
