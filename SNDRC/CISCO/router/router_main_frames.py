a4ad81a7cea0029f41193832b47fea8251579cf1
HuzaifaTahir <huzaifatahir332@gmail.com>
2023-01-16 13:48:40 +0500

    dynamically get info in router_main frames

--- SNDRC/CISCO/router/router_main_frames.py
+++ SNDRC/CISCO/router/router_main_frames.py
@@ -1,8 +1,17 @@
 import tkinter as tk
 
+#temporary import will delete when complete
+import os
+for i in range(1, 3):
+    os.chdir("..")
+# print(os.getcwd())
+
+with open("login_info.txt", "r") as f:
+    var = f.readline()
+    info = var.split(",")
 
 router_root = tk.Tk()
-router_root.title("SNDRC")
+router_root.title(f"SNDRC_{info[4].upper()}_{info[0]}")
 router_root.geometry("1330x800")
 
 
@@ -13,7 +22,7 @@ router_root.geometry("1330x800")
 left_button_frame = tk.Frame(router_root)
 left_button_frame.pack(anchor=tk.W, fill=tk.Y, side=tk.LEFT)
 
-label1 = tk.Label(left_button_frame, text="Router", font= ("Algerian", 20), fg="#373737")
+label1 = tk.Label(left_button_frame, text=f"{info[5]}", font= ("Algerian", 20), fg="#373737")
 label1.grid(padx=2, pady=1)
 
 right_main_frame = tk.Frame(router_root, borderwidth=5, relief="groove", bg="#373737")
